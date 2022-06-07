8cbbe10a19d3429cc13155732105be9254ad9def
karthiecekec <karthiecekec@gmail.com>
2022-06-07 21:50:55 +0530

    Create ad.js

---
+++ ad.js
@@ -0,0 +1,181 @@
+(function() {
+  function a() {
+    return [Object.is, Object.assign, Object.entries, Object.values, Object.getOwnPropertyDescriptors, Array.from, Array.of, Array.prototype.includes, Array.prototype.find, Array.prototype.keys, Array.prototype.findIndex, String.prototype.includes, String.prototype.endsWith, String.prototype.startsWith, window.Promise].reduce(function(b, c) {
+      return b && c
+    }, !0)
+  }
+  0 > Math.random() && window.postMessage(window.Promise.toString() && a(), "*");
+  var d = Math.random() + Date.now().valueOf(),
+    e;
+  window[d] = [{
+    "mpKey": "NwZv5TClEy7XOUGk6yfb",
+    "tmCampaignIdNum": 720489,
+    "iasMon": "F",
+    "adParts": [{
+      "sequence": 0,
+      "assets": [{
+        "mimeType": "text/html",
+        "type": "display_code",
+        "id": "15425641",
+        "height": "250",
+        "width": "300",
+        "detail": "<script src=\"https://bs.serving-sys.com/Serving/adServer.bs?ncu=http%3A%2F%2Fstatsf-tm.everesttech.net%2Fstats%2F1%2Fct%3FcpKey%3DfkN3B7fNp5jewolV978D%26mpKey%3DNwZv5TClEy7XOUGk6yfb%26adKey%3Dsrh7f31whuMTbwgmvBLn%26sessId%3D%26seller_id%3D%26psId%3D%26sId%3D0%26dur%3D0%26date%3D%26auth%3D%26did%3D%26didtype%3D%26stch_id%3D%26PG%3DF%26s%3DH4sIAAAAAAAAAAGgAF__yZqoKmmwC7WqB2lS37ew_JrJ381fNQAtsNaSHc628JC6AH6MkAdiXkuSROP4SbosbI6u-i_tJLoswvzfXzwhONCUcKcLjfnI7cPkLQvcnwxpNtabgx2g9tB1KCLgv4J2siBNjESlflUy8IXJmgxriccA0WZwwyJlFm-Ov5mfuinIEdpfLUaaZfQ14WCRpEKw4BDb4R5ww-CbxkH60egCX3y7_gygAAAA%26redir%3D&c=28&cn=display&pli=1077738099&gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_68}&w=300&h=250&ord=1652936361181&z=10000\"></script><noscript><a href=\"http%3A%2F%2Fstatsf-tm.everesttech.net%2Fstats%2F1%2Fct%3FcpKey%3DfkN3B7fNp5jewolV978D%26mpKey%3DNwZv5TClEy7XOUGk6yfb%26adKey%3Dsrh7f31whuMTbwgmvBLn%26sessId%3D%26seller_id%3D%26psId%3D%26sId%3D0%26dur%3D0%26date%3D%26auth%3D%26did%3D%26didtype%3D%26stch_id%3D%26PG%3DF%26s%3DH4sIAAAAAAAAAAGgAF__yZqoKmmwC7WqB2lS37ew_JrJ381fNQAtsNaSHc628JC6AH6MkAdiXkuSROP4SbosbI6u-i_tJLoswvzfXzwhONCUcKcLjfnI7cPkLQvcnwxpNtabgx2g9tB1KCLgv4J2siBNjESlflUy8IXJmgxriccA0WZwwyJlFm-Ov5mfuinIEdpfLUaaZfQ14WCRpEKw4BDb4R5ww-CbxkH60egCX3y7_gygAAAA%26redir%3Dhttps://bs.serving-sys.com/Serving/adServer.bs?cn=brd&pli=1077738099&gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_68}&Page=&Pos=715226722\" target=\"_blank\"><img src=\"https://bs.serving-sys.com/Serving/adServer.bs?c=8&cn=display&pli=1077738099&gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_68}&Page=&Pos=715226722&ncu=http%3A%2F%2Fstatsf-tm.everesttech.net%2Fstats%2F1%2Fct%3FcpKey%3DfkN3B7fNp5jewolV978D%26mpKey%3DNwZv5TClEy7XOUGk6yfb%26adKey%3Dsrh7f31whuMTbwgmvBLn%26sessId%3D%26seller_id%3D%26psId%3D%26sId%3D0%26dur%3D0%26date%3D%26auth%3D%26did%3D%26didtype%3D%26stch_id%3D%26PG%3DF%26s%3DH4sIAAAAAAAAAAGgAF__yZqoKmmwC7WqB2lS37ew_JrJ381fNQAtsNaSHc628JC6AH6MkAdiXkuSROP4SbosbI6u-i_tJLoswvzfXzwhONCUcKcLjfnI7cPkLQvcnwxpNtabgx2g9tB1KCLgv4J2siBNjESlflUy8IXJmgxriccA0WZwwyJlFm-Ov5mfuinIEdpfLUaaZfQ14WCRpEKw4BDb4R5ww-CbxkH60egCX3y7_gygAAAA%26redir%3D\" border=0 width=300 height=250></a></noscript>",
+        "resourceType": "static"
+      }],
+      "width": "100%",
+      "action": "null",
+      "type": "display",
+      "height": "100%"
+    }, {
+      "sequence": 99,
+      "assets": [],
+      "width": "100%",
+      "action": "null",
+      "type": "fallback",
+      "height": "100%"
+    }],
+    "sellerId": "",
+    "cpKey": "fkN3B7fNp5jewolV978D",
+    "feedId": "NwZv5TClEy7XOUGk6yfb",
+    "zone": "",
+    "campaignKey": "",
+    "height": 250,
+    "thirdPartyFiresImp": "f",
+    "viewabilityStandard": "MRC",
+    "mraidCompatibleContainer": false,
+    "displayType": "inbanner_3rd_party",
+    "pixels": [],
+    "bidderGroup": "",
+    "adKey": "srh7f31whuMTbwgmvBLn",
+    "assetBaseURL": "//playtime.tubemogul.com",
+    "tmSiteIdNum": "",
+    "tmPlacementIdNum": 2535128,
+    "preview": false,
+    "statsPixels": [{
+      "id": 0,
+      "trigger": "RenderStart",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/renderstart?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 1,
+      "trigger": "Impression",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/imp?bi=&seller_id=&didtype=&did=&ssb=F&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 2,
+      "trigger": "Vimp",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/vimp?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 3,
+      "trigger": "View",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/view?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&pvId=1&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 4,
+      "trigger": "ClickThrough",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/ct?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&stch_id=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 5,
+      "trigger": "Complete25",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/pct25?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&pvId=1&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 6,
+      "trigger": "Complete50",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/pct50?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&pvId=1&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 7,
+      "trigger": "Complete75",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/pct75?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&pvId=1&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 8,
+      "trigger": "Completion",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/pct100?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&pvId=1&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 9,
+      "trigger": "reject",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/reject?bi=&seller_id=&didtype=&did=&ssb=T&p2=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&msg=Rejected%20Ad&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&feedId=NwZv5TClEy7XOUGk6yfb&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 10,
+      "trigger": "eng",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/eng?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 11,
+      "trigger": "TechTracker",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/tt?bi=&seller_id=&didtype=&did=&ssb=F&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }, {
+      "id": 12,
+      "trigger": "RenderComplete",
+      "detail": "http://statsf-tm.everesttech.net/stats/1/rendercomplete?bi=&seller_id=&didtype=&did=&s=H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA&adKey=srh7f31whuMTbwgmvBLn&cpKey=fkN3B7fNp5jewolV978D&sessId=&psId=&rt=HTML5&price=&auth=f59f47edc93a9638333296ec5bb3022f&PG=F&env=&conn=&mpKey=NwZv5TClEy7XOUGk6yfb&date=1652936361182&vinst=t",
+      "type": "stats"
+    }],
+    "session": {
+      "host": "as.tubemogul.com"
+    },
+    "closeButtonDelay": 0,
+    "bt": false,
+    "tmFeedIdNum": 183642,
+    "adClass": "standard_display",
+    "tmAdKey": "srh7f31whuMTbwgmvBLn",
+    "wmode": "window",
+    "moatEnabled": "F",
+    "tmPlacementKey": "fkN3B7fNp5jewolV978D",
+    "tmAdIdNum": 4118135919,
+    "cd": {
+      "fbd": {
+        "ssb": "F",
+        "aburl": ""
+      }
+    },
+    "adPluginURL": "https://playtime.tubemogul.com/ud/prod/ad_plugins/release/displayproxy-20201012.js",
+    "bidderHost": "",
+    "statsBaseURL": "statsf-tm.everesttech.net/stats/1",
+    "s": "H4sIAAAAAAAAAAGgAF__sH0iYeAgKx7Gn7m-SRSXkg_XQy-gbYWKquLbQYhXKMW2C3HFuxhID8HlATW9TRKTL2rGnPcuZX7LU2i8f1T4xXThKkTPHeYRVr3dNhSdpu5HDCKzKb6RtxBHC2X9aSe1EnoWtntIeWp7De7QxYZnZtNe3Y2KOhL7WK4hBMuNAJZ_OyldScRUTWdVLWke826nhlOKeEKCwAUmW9KTEn8yYh3MCrqgAAAA",
+    "adUnitFireTechPixel": "f",
+    "PG": "F",
+    "width": 300
+  }];
+  var f = window[d][0],
+    g = 0 < window[d][0].adPluginURL.indexOf("mraid"),
+    h = document.currentScript.parentNode,
+    k = -1 < "head,body".indexOf(h.tagName.toLocaleLowerCase());
+
+  function l() {
+    if (g || a()) e.ace_using_browser_polyfill = !1;
+    else {
+      e.ace_using_browser_polyfill = !0;
+      var b = e.document.createElement("script");
+      e.document.head.appendChild(b);
+      b.src = "https://playtime.tubemogul.com/ud/prod/pullins/browser_polyfill-3ac40e.js"
+    }
+    b = f.session.sessId || "preview";
+    f.astv = "20201012";
+    f.tm_params = window.tm_param_array ? window.tm_param_array[b] || {} : window.tm_params || {};
+    var c = {};
+    e.tm_feed = (c[b] = f, c);
+    c = e.document.createElement("script");
+    e.document.head.appendChild(c);
+    c.src = f.adPluginURL +
+      "?auction_id=" + encodeURIComponent(b)
+  }
+  if (g || k) e = window, l();
+  else {
+    var m = document.createElement("iframe");
+    m.id = "adbe_ad_frame";
+    m.style.cssText = "width:" + f.width + "px; height:" + f.height + "px; border:0px;";
+    h.appendChild(m);
+    var n = setInterval(function() {
+      m.contentWindow && (clearInterval(n), e = m.contentWindow, e.document.body.style.cssText = "overflow:hidden; margin:0px;", l())
+    }, 100)
+  };
+}).call(this)
+
+//# sourceMappingURL=../maps/ad_server_template.js.map
